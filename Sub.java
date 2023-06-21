faa3d3f5c395f04ab4e8a7fc1afb2ee10093e471
gsr1trade <hama32915@gmail.com>
2023-06-21 23:18:14 +0900

    コミット

---
+++ Sub.java
@@ -0,0 +1,8 @@
+public class Sub {
+  public static void main(String[] args) {
+    String[] names = { "阿田", "井田", "宇田", "江田", "小田" };
+    for(int i = 0; i < names.length; i++) {
+    System.out.print(names[i] + "");
+    }
+  }
+}
