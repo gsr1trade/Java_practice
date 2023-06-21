5e197e6a971f007c651f3a528521e71b71a93774
gsr1trade <hama32915@gmail.com>
2023-06-21 22:38:33 +0900

    配列のコミット（GSET）

---
+++ Yoso.java
@@ -0,0 +1,43 @@
+public class Yoso {
+  public static void main(String[] args){
+    
+    // int[] valueArray1 = {1,2,3};
+    // int[] valueArray2 = null;
+    // System.out.print(valueArray1.length);
+    // System.out.print(valueArray2.length);
+
+      // String[] array;
+      //   array = new String[3];
+      //   array[0] = "地主";
+      //   array[1] = "黒川";
+      //   array[2] = "金井";
+
+      String[] array = new String[3];
+         array[0] = "地主";
+         array[1] = "黒川";
+         array[2] = "金井";
+
+      // String[] array = {"","",""};
+      
+
+    // String[] array = {"k","え","q"};
+    // int value = array.length;
+    // System.out.println(value);
+
+
+    //実行時エラー「NullPointerException」
+
+    // int value1 = 10;
+    // int value2 = value1;
+    // value2 = 20;
+    // System.out.print("value1=" + value1);
+    // System.out.print("value2=" + value2);
+
+    // String[] array = {"地主","黒川","金井"};
+    // array[1] = "竹中";
+    // System.out.print(array[1]);
+
+    // String value = array[1];
+    // System.out.print(value);
+  }
+}
