a82c316258cad8450f98eaf8f7b22629c9d1a447
gsr1trade <hama32915@gmail.com>
2023-06-21 23:17:44 +0900

    メソッド呼び出し

---
+++ research.java
@@ -0,0 +1,13 @@
+public class research {
+  public static void main(String[] args){
+    System.out.println("start");
+    kfa();
+    System.out.println("end");
+  }
+
+  public static void kfa(){
+    System.out.println("あ");
+    System.out.println("い");
+    System.out.println("う");
+  }
+}
